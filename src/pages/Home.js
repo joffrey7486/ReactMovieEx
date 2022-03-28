16d55d53733af5d1cea5fb185e88852a639aba9e
caugnon <joffreycaugnon@gmail.com>
2022-03-28 13:35:06 +0200

    header form & card without userList button

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from '../component/Form';
+import Header from '../component/Header';
+
+const Home = () => {
+    return (
+        <div className='home-page'>
+            <Header />
+            <Form />
+        </div>
+    );
+};
+
+export default Home;
