5883fb34a05533dcdce9331a23f7de59cad2d99d
caugnon <joffreycaugnon@gmail.com>
2022-03-28 14:03:08 +0200

    logical search

--- src/component/Form.js
+++ src/component/Form.js
@@ -4,18 +4,24 @@ import Card from './Card';
 
 const Form = () => {
     const [moviesData, setMoviesData] = useState([]);
+    const [search, setSearch] = useState('lord');
 
     useEffect(() => {
-        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=1447783824b0bc8f1f54fa0a83221be2&query=lord&language=fr-FR`)
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=1447783824b0bc8f1f54fa0a83221be2&query=${search}&language=fr-FR`)
             .then((res) => setMoviesData(res.data.results))
-    }, []);
+    }, [search]);
 
 
     return (
         <div className='form-component'>
             <div className="form-container">
                 <form>
-                    <input type="text" placeholder="Entrer le titre d'un film" id='search-input' />
+                    <input 
+                        type="text" 
+                        placeholder="Entrer le titre d'un film" 
+                        id='search-input' 
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
                     <input type='submit' value="Rechercher" />
                 </form>
 
