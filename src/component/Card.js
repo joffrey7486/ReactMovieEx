1d4ba1b32233f2ae93035f7e3c006a443f0eb8ac
caugnon <joffreycaugnon@gmail.com>
2022-03-29 09:32:55 +0200

    deleting userlist

--- src/component/Card.js
+++ src/component/Card.js
@@ -80,6 +80,11 @@ const Card = ( { movie } ) => {
             window.localStorage.movies = storedData;
         } 
     }
+    const deleteStorage = () => {
+        let storedData = window.localStorage.movies.split(",");
+        let newData = storedData.filter(id => id != movie.id);
+        window.localStorage.movies = newData;
+    }
 
     return (
         <div className='card'>
@@ -93,14 +98,21 @@ const Card = ( { movie } ) => {
 
             <h4>{movie.vote_average}/10 <span>⭐</span></h4>
 
-            <ul>{movie.genre_ids ? genreFinder() : movie.genres.map((genre) => (
-                <li>{genre.name}</li> 
+            <ul>{movie.genre_ids ? genreFinder() : movie.genres.map((genre, index) => (
+                <li key={index}>{genre.name}</li> 
             ))}</ul>
 
             {movie.overview ? <h3>Synopsis</h3> : ""}
             <p>{movie.overview}</p>
 
-            <div className="btn" onClick={() => addStorage()}>Ajouter au coup de coeur</div>
+            {movie.genre_ids ? (
+                <div className="btn" onClick={() => addStorage()}>Ajouter au coup de coeur</div>
+            ) : (
+                <div className="btn" onClick={() => {
+                    deleteStorage(); 
+                    window.location.reload();
+                }}>Supprimer de la liste</div>
+            )}
         </div>
     );
 };
