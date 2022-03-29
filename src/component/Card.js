e2430cce8aaf4c909e271d7fe548f601b9a71442
caugnon <joffreycaugnon@gmail.com>
2022-03-29 08:26:06 +0200

    adding userlist

--- src/component/Card.js
+++ src/component/Card.js
@@ -72,6 +72,14 @@ const Card = ( { movie } ) => {
         }  
         return genreArray.map((genre) => <li key={genre}>{genre}</li>);
     }
+    const addStorage = () => {
+        let storedData = window.localStorage.movies ? window.localStorage.movies.split(",") : [];
+
+        if (!storedData.includes(movie.id.toString())) {
+            storedData.push(movie.id);
+            window.localStorage.movies = storedData;
+        } 
+    }
 
     return (
         <div className='card'>
@@ -85,12 +93,14 @@ const Card = ( { movie } ) => {
 
             <h4>{movie.vote_average}/10 <span>⭐</span></h4>
 
-            <ul>{genreFinder()}</ul>
+            <ul>{movie.genre_ids ? genreFinder() : movie.genres.map((genre) => (
+                <li>{genre.name}</li> 
+            ))}</ul>
 
             {movie.overview ? <h3>Synopsis</h3> : ""}
             <p>{movie.overview}</p>
 
-            <div className="btn">Ajouter au coup de coeur</div>
+            <div className="btn" onClick={() => addStorage()}>Ajouter au coup de coeur</div>
         </div>
     );
 };
--- src/pages/UserList.js
+++ src/pages/UserList.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import Card from '../component/Card';
 import Header from '../component/Header';
 
 const UserList = () => {
+    const [listData, setListData] = useState([]);
+
+    useEffect(() => {
+        let moviesId = window.localStorage.movies ? window.localStorage.movies.split(",") : [];
+
+        for (let i = 0; i < moviesId.length; i++) {
+            axios
+            .get(`https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=1447783824b0bc8f1f54fa0a83221be2&language=fr-FR`)
+            .then((res) => setListData((listData) => [...listData, res.data])) 
+        }
+    }, []);
+
     return (
-        <div>
+        <div className='user-list-page'>
             <Header />
+            <h2>Coup de coeur <span>💖</span></h2>
+            <div className="result">
+                {listData.length > 0 ? listData.map((movie) => (
+                    <Card movie={movie} key={movie.id} />
+                )) : <h2>Vous n'avez pas encore de films dans votre liste de coup de coeur</h2>}
+            </div>
         </div>
     );
 };
